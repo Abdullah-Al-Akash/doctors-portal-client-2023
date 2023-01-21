7cf2fda9709d691db837b9290e850de27cc67307
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-01-21 15:26:39 +0600

    Complete Make Appointment Section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import DentalCare from '../DentalCare/DentalCare';
 import InfoCards from '../InfoCards/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 
 const Home = () => {
         return (
-                <div className="mx-5">
-                        <Banner></Banner>
-                        <InfoCards></InfoCards>
-                        <Services></Services>
-                        <DentalCare></DentalCare>
-                </div>
+                <>
+                        <div className="mx-5">
+                                <Banner></Banner>
+                                <InfoCards></InfoCards>
+                                <Services></Services>
+                                <DentalCare></DentalCare>
+                        </div>
+                        <MakeAppointment></MakeAppointment>
+                </>
         );
 };
 
