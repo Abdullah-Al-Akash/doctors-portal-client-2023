8e43f6b692a11516470177be6420f1d1a69fcc6f
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-01-23 20:41:42 +0600

    Login Form Created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+        const { register, handleSubmit } = useForm();
+        const [data, setData] = useState("");
         return (
-                <div>
-                        <h2>This is  Login</h2>
-                </div>
+                <section className="h-[650px] flex justify-center items-center">
+                        <div>
+                                <h2 className='text-center font-bold text-3xl text-accent py-4'>Login Here</h2>
+                                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                                        <div className="form-control w-full ">
+                                                <label className="label">
+                                                        <span className="label-text">Enter Your Email</span>
+                                                </label>
+                                                <input {...register("email")} type="email" placeholder="Email" className="input input-bordered w-full " />
+                                                <label className="label">
+                                                        <span className="label-text">Enter Your Password</span>
+                                                </label>
+                                                <input {...register("password")} type="password" placeholder="Password" className="input input-bordered w-full " />
+
+                                                <input className='btn btn-accent text-white mt-4' type="submit" />
+                                        </div>
+
+
+                                        <p>{data}</p>
+                                </form>
+                        </div>
+                </section>
         );
 };
 
